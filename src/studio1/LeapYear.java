3334f7400f69df635562476d0e3d669853d30ae8
Ethan-12-alt <ethanwu@wustl.edu>
2026-01-21 14:26:36 -0600

    refined

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -4,14 +4,12 @@ import java.util.Scanner;
 
 public class LeapYear {
     public static void main(String []args){
-        boolean jus = true;
+        
         Scanner in = new Scanner(System.in);
 		int numYear = in.nextInt();
-		if (numYear % 4==0 && numYear % 100 != 0) {
-			System.out.println(numYear+" is a leap year : "+ jus);
-		}else {
-			jus = false;
-			System.out.println(numYear+" is a leap year : "+ jus);
-		}
-    }
+		
+            boolean isLeapYear = (numYear % 4 == 0 && (numYear % 100 != 0 || numYear % 400 == 0));
+            System.out.println(numYear + " is a leap year: " + isLeapYear);
+        }
 }
+200
